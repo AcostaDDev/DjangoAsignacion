51d49d331df980f1950200b7e02e3c5fbe0aace3
AcostaDDev <acostadavdevelopment>
2024-03-23 16:36:29 +0100

    Login hecho

--- Alumnos/views.py
+++ Alumnos/views.py
@@ -1,15 +1,17 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 from django.urls import reverse
-from .models import Alumno, Proyectos
+from .models import Alumno
 # Create your views here.
 
+@login_required
 def index(request):
     alumnos = Alumno.objects.order_by('nombre')
     variables = {'alumnos':alumnos}
     return render(request, 'Alumnos/index.html', variables)
 
 
+@login_required
 def alumno(request, alumno_id):
     alumno = Alumno.objects.get(id=alumno_id)
     variables = {
